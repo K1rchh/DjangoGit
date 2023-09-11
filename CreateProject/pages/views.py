b137bb76cae7d61a2ebaa916428b5a7bc2ebc945
K1rch <K1rch@bk.ru>
2023-09-12 00:35:43 +0500

    fix

--- CreateProject/pages/views.py
+++ CreateProject/pages/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homePageView(request):
-    return HttpResponse('Hello, nigereeeeeeeeeeeeeeeeeeee!')
+    return HttpResponse('Hello, World!')
