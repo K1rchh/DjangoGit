a8dd4f10e24a522ca2b95d72baf89fcdec311741
K1rchh <144620806+K1rchh@users.noreply.github.com>
2023-09-12 00:34:56 +0500

    Update views.py

--- CreateProject/pages/views.py
+++ CreateProject/pages/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homePageView(request):
-    return HttpResponse('Hello, nigere!')
+    return HttpResponse('Hello, nigereeeeeeeeeeeeeeeeeeee!')
